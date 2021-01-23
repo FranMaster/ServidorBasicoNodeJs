78799d587e2209b100f7694df6d2851d5e93f626
Francisco Jimenez <franjicohen@gmail.com>
2021-01-23 19:00:54 -0300

    se emula validacion de token y base de datos

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 const express=require('express')
 const app=express()
+app.use(express.json())
 let PORT=8000
-
-
+const tokenAuth=569
+const dbUsuarios=[  
+    {
+        id:1,
+        user:'Fran',
+        pass:123456,
+        userName:'Francisco Jimenez'
+    },
+    {
+        id:2,
+        user:'Fede',
+        pass:1234,
+        userName:'Federico Enrriquez'
+    }
+]
 //http://localhost:8000/saludo [GET]
 app.get('/saludo',(req,res)=>{
 
@@ -10,11 +24,33 @@ app.get('/saludo',(req,res)=>{
 
 })
 
-//http://localhost:8000/saludo [GET]
+//http://localhost:8000/saludo [POST]
 app.post('/saludo',(req,res)=>{
 
     res.status(200).json({mensaje:'Hola Fede soy POST'})
 
+})
+
+//http://localhost:8000/login [POST]
+app.post('/login',(req,res)=>{
+
+    let cabecera=req.headers
+    let body=req.body
+
+    if(cabecera.auth && cabecera.auth==tokenAuth ){
+        if(body && body.user && body.pass){
+           let resultados=dbUsuarios.filter(item=>(item.user==body.user && item.pass==body.pass))
+           if (resultados.length>0){
+            res.status(200).json({mensaje:`Bienvenido ${resultados[0].userName}` })
+           }else{
+            res.status(404).json({mensaje:'Usuario o contraseña errada'})    
+           }
+        }else{
+            res.status(400).json({mensaje:'llene todos los campos'})
+        }
+    }else{
+        res.status(401).json({mensaje:'No estas Autorizado'})
+    }
 })
 
 
